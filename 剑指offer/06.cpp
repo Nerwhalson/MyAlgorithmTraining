02b8864986408d4de1a08cbcb4e5a5932796281c
Oasis42 <1547150696@qq.com>
2022-09-04 20:50:41 +0800

    jianzhi offer 06

---
+++ 剑指offer/06.cpp
@@ -0,0 +1,19 @@
+/**
+ * Definition for singly-linked list.
+*/
+#include <vector>
+
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+    vector<int> reversePrint(ListNode* head) {
+
+    }
+};
