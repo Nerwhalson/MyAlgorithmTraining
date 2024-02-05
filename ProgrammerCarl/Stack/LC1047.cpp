0f970612b7d35bef2caabb85227b5df1ba86d63b
Hou Ke <hou_ke@outlook.com>
2024-02-05 23:49:24 +0100

    add lc 1047

---
+++ ProgrammerCarl/Stack/LC1047.cpp
@@ -0,0 +1,13 @@
+#include <stack>
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+    string removeDuplicates(string s) {
+        string ans = "";
+        stack<char> buff;
+        return ans;
+    }
+};
