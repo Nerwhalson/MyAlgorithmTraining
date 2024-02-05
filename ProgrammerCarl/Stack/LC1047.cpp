cb6991be5fc9bbfd9e3aa52ca9aced990a54c90c
Hou Ke <hou_ke@outlook.com>
2024-02-05 23:55:57 +0100

    update lc 1047

--- ProgrammerCarl/Stack/LC1047.cpp
+++ ProgrammerCarl/Stack/LC1047.cpp
@@ -7,7 +7,24 @@ class Solution {
 public:
     string removeDuplicates(string s) {
         string ans = "";
-        stack<char> buff;
+        stack<char> buff1, buff2;
+        for(auto i : s){
+            buff1.push(i);
+        }
+        while(!buff1.empty()){
+            if(buff2.empty() || buff2.top() != buff1.top()){
+                buff2.push(buff1.top());
+                buff1.pop();
+            }
+            else{
+                buff1.pop();
+                buff2.pop();
+            }
+        }
+        while(!buff2.empty()){
+            ans.push_back(buff2.top());
+            buff2.pop();
+        }
         return ans;
     }
 };
